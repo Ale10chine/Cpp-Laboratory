b39fe3ca33f97393b0ce1dfb498c5cfc00d9bc33
Ale10chine <alex10.chinello@gmail.com>
2024-08-12 13:18:48 +0200

    prova gitingore

--- Lab1/HelloWorld.cpp
+++ Lab1/HelloWorld.cpp
@@ -1,78 +1,82 @@
 #include <iostream>
-#include <vector>                 //libreria per vettori
+#include <vector> //libreria per vettori
 using namespace std;
 
-int g  = 1;                       // una variabile int globale (in quanto globale, è anche statica senza bisogno di esplicitarlo) inizializzata a un valore diverso da 0
-int g2;                           // variabile int globale non inizizalizzata
+int g = 1; // una variabile int globale (in quanto globale, è anche statica senza bisogno di esplicitarlo) inizializzata a un valore diverso da 0
+int g2;    // variabile int globale non inizizalizzata
 
-
-void print(string s, vector<int> v);    //Definizione funzioni
-void fibonacci(int x, int y, vector<int>& v, int n);
+void print(string s, vector<int> v); // Definizione funzioni
+void fibonacci(int x, int y, vector<int> &v, int n);
 void call_counter(void);
 
+//
+int main(void)
+{
 
-int main(void){
-    
-	cout<<("Hello World\n");
-    
-    int a;                         //variabile int locale automatica
-    int static b = 1;              //variabile int locale statica inizizalizzata a un valore diverso da 0
-    int static c;                  // variabile int locale statica non inizizlizzata
-    
-    vector<int> v1 {1,2,3,4,5};
-    print("il mio vettore e'",v1);
-    
+    cout << ("Hello World\n");
+
+    int a;            // variabile int locale automatica
+    int static b = 1; // variabile int locale statica inizizalizzata a un valore diverso da 0
+    int static c;     // variabile int locale statica non inizizlizzata
+
+    vector<int> v1{1, 2, 3, 4, 5};
+    print("il mio vettore e'", v1);
+
     vector<int> fib;
-    int x,y,n;
-    
-    cout<<"Inserisci i due numeri di partenza consecutivi della sequenza di fibonacci\n";
-    cin>>x;
-    cin>>y;
-    cout<<"Quanti n primi valori della sequenza di fibonacci a partire dai due valori inseriti vuoi stampare?\n";
-    cin>>n; cout<<"\n";
-    
-    fibonacci(x,y,fib,n);
-    print("Vettore di fibonacci",fib);
-    
-    
-    
+    int x, y, n;
+
+    cout << "Inserisci i due numeri di partenza consecutivi della sequenza di fibonacci\n";
+    cin >> x;
+    cin >> y;
+    cout << "Quanti n primi valori della sequenza di fibonacci a partire dai due valori inseriti vuoi stampare?\n";
+    cin >> n;
+    cout << "\n";
+
+    fibonacci(x, y, fib, n);
+    print("Vettore di fibonacci", fib);
+
     call_counter();
     call_counter();
     call_counter();
     call_counter();
     call_counter();
-	return 0;
+    return 0;
 }
 
-void print(string s, vector<int> v){
-    cout<< s <<":\n\n";
-    for(int i = 0; i<v.size(); i++){      
-        cout<<"v["<<i<<"] ="<<v[i]<<"\n";
-        } 
-    cout<<"\n";
+void print(string s, vector<int> v)
+{
+    cout << s << ":\n\n";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << "v[" << i << "] =" << v[i] << "\n";
+    }
+    cout << "\n";
 }
 
-void fibonacci(int x, int y, vector<int>& v, int n){
+void fibonacci(int x, int y, vector<int> &v, int n)
+{
     v.push_back(x);
     v.push_back(y);
-     
-     if (n == 0 or n == 1) return;
-    
+
+    if (n == 0 or n == 1)
+        return;
+
     int somma = 0;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         somma = x + y;
         y = x;
         x = somma;
         v.push_back(somma);
-        }
-     return;
     }
-    
-void call_counter(void){
-   static int c = 0;                                 //essendo n statico si aggiorna ogni volta ?
-   cout<<"Numero di chiamate funzione call_counter : "<<++c<<"\n";    
+    return;
+}
 
-    }
+void call_counter(void)
+{
+    static int c = 0; // essendo n statico si aggiorna ogni volta ?
+    cout << "Numero di chiamate funzione call_counter : " << ++c << "\n";
+}
 
 // a. in quale delle sezioni riportate da size compare la variabile locale automatica? Perché?
 // Non compare in nessuna sezione perché è una variabile che si trova nell'RA del main.
